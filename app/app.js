68556083c2ebf85224bc3ec51181571d1201c867
Jinwoo Jung <jinwoo0302@naver.com>
2023-12-23 17:00:55 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express=require("express");
+const bodyParser=require("body-parser");
 const app=express();
 
 // 라우팅
@@ -12,6 +13,9 @@ const home=require("./src/routes/home");
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// URL통해 전달되는 데이터에 한글, 공백 등 문자 포함 시 제대로 인식되지 않는 에러 해결
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use("/",home);
 
