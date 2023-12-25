e157d6a47b247df854dfab0a24321104fded8c13
Jinwoo Jung <jinwoo0302@naver.com>
2023-12-25 21:19:18 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,23 @@
 "use strict";
 
+const fs=require("fs").promises;
+
 class UserStorage {
-    static #users={
-        id:["jinwoo", "jinwoo00", "jinwoo0302"],
-        psword:["1234","1234","1234"],
-        name:["진우", "진우00","진우32"],
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
+            
+        const idx=users.id.indexOf(id);
+        const userKeys=Object.keys(users); // [id, psword, name]
+        const userInfo=userKeys.reduce((newUser, info)=>{
+            newUser[info]=users[info][idx];
+            return newUser;
+        },{}); //이때 {}는 newUser의 초깃값
+        return userInfo;
     };
 
+
     static getUsers(...fields){
-        const users=this.#users;
+        // const users=this.#users;
         const newUsers=fields.reduce((newUsers, field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field]=users[field];
@@ -20,19 +29,17 @@ class UserStorage {
     };
 
     static getUserInfo(id){
-        const users=this.#users;
-        const idx=users.id.indexOf(id);
-        const userKeys=Object.keys(users); // [id, psword, name]
-        const userInfo=userKeys.reduce((newUser, info)=>{
-            newUser[info]=users[info][idx];
-            return newUser;
-        },{}); //이때 {}는 newUser의 초깃값
+        return fs.readFile("./src/databases/users.json")
+        .then((data)=>{
+            return this.#getUserInfo(data,id);
+        })
+        .catch(console.error);
 
-        return userInfo;
     };
 
+
     static save(userInfo){
-        const users=this.#users;
+        // const users=this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
