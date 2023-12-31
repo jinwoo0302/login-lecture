f2d7bba0498675576469d646544792c4593f0549
Jinwoo Jung <jinwoo0302@naver.com>
2023-12-31 11:37:57 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -1,26 +1,30 @@
 "use strict";
 
 // 모듈
-const express=require("express");
-const bodyParser=require("body-parser");
-const dotenv=require("dotenv");
-dotenv.config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
-const app=express();
+
+// const morgan = require("morgan");
+// const accessLogStream=require("./src/config/log");
+
+const app = express();
+dotenv.config();
 
 
 // 라우팅
-const home=require("./src/routes/home");
-
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL통해 전달되는 데이터에 한글, 공백 등 문자 포함 시 제대로 인식되지 않는 에러 해결
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(morgan("dev"));
+// app.use(morgan("common",{stream: accessLogStream}));
+app.use("/", home);
 
-app.use("/",home);
-
-module.exports=app;
+module.exports = app;
