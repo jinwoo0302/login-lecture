e1f4a9f30abf7b28aa138567b4887ecd99e33092
Jinwoo Jung <jinwoo0302@naver.com>
2023-12-31 19:29:24 +0900

    백엔드 맛보기 최종 최적화

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // 모듈
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const logger=require("./src/config/logger")
 const dotenv = require("dotenv");
 
 
@@ -23,7 +21,6 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL통해 전달되는 데이터에 한글, 공백 등 문자 포함 시 제대로 인식되지 않는 에러 해결
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(morgan("tiny",{stream: logger.stream}));
 
 app.use("/", home);
 
